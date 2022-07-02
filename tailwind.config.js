d4bd91877cb01313e5df0dc01f7fa86c9db2dc1b
parisheelan <parisheelan@gmail.com>
2022-07-02 23:12:35 +0530

    Navbar

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+      colors: {
+        red: 'hsl(12,88%,58%)'
+      }
+    },
+  },
+  plugins: [],
+}
